9b2e3414cf803969570f4bbfaac03a9abf673f9b
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2023-07-15 22:21:48 +0300

    Update Selects

--- src/material/react/M3FilledSelect.tsx
+++ src/material/react/M3FilledSelect.tsx
@@ -2,13 +2,37 @@ import * as React from "react";
 import FilledSelect from "material/web/MdFilledSelect";
 
 interface M3FilledSelectProps {
-  children?: React.ReactNode;
+  quick?: boolean;
+  required?: boolean;
   disabled?: boolean;
+  errorText?: string;
+  label?: string;
+  supportingText?: string;
+  error?: boolean;
+  menuFixed?: boolean;
+  typeaheadDelay?: number;
+  displayText?: string;
+  children?: React.ReactNode;
 }
 
 export default function M3FilledSelect({
-  children,
-  disabled
+  quick,
+  required,
+  disabled,
+  errorText,
+  label,
+  supportingText,
+  error,
+  menuFixed,
+  typeaheadDelay,
+  displayText,
+  children
 }: M3FilledSelectProps) {
-  return <FilledSelect disabled={disabled}>{children}</FilledSelect>;
+  return <FilledSelect
+    quick={quick} required={required} disabled={disabled} errorText={errorText} label={label}
+    supportingText={supportingText} error={error} menuFixed={menuFixed} typeaheadDelay={typeaheadDelay}
+    displayText={displayText}
+  >
+    {children}
+  </FilledSelect>;
 }
--- src/material/react/M3OutlinedSelect.tsx
+++ src/material/react/M3OutlinedSelect.tsx
@@ -2,13 +2,37 @@ import * as React from "react";
 import OutlinedSelect from "material/web/MdOutlinedSelect";
 
 interface M3OutlinedSelectProps {
+  quick?: boolean;
+  required?: boolean;
   disabled?: boolean;
+  errorText?: string;
+  label?: string;
+  supportingText?: string;
+  error?: boolean;
+  menuFixed?: boolean;
+  typeaheadDelay?: number;
+  displayText?: string;
   children?: React.ReactNode;
 }
 
 export default function M3OutlinedSelect({
+  quick,
+  required,
   disabled,
+  errorText,
+  label,
+  supportingText,
+  error,
+  menuFixed,
+  typeaheadDelay,
+  displayText,
   children
 }: M3OutlinedSelectProps) {
-  return <OutlinedSelect disabled={disabled}>{children}</OutlinedSelect>;
+  return <OutlinedSelect
+    quick={quick} required={required} disabled={disabled} errorText={errorText} label={label}
+    supportingText={supportingText} error={error} menuFixed={menuFixed} typeaheadDelay={typeaheadDelay}
+    displayText={displayText}
+  >
+    {children}
+  </OutlinedSelect>;
 }
