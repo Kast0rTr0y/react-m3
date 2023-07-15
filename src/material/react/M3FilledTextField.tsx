c5daee061957dd763b95a44e9361df535f03fb20
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2023-07-15 23:09:22 +0300

    Update Text fields

--- src/material/react/M3FilledTextField.tsx
+++ src/material/react/M3FilledTextField.tsx
@@ -2,10 +2,31 @@ import * as React from "react";
 import FilledTextField from "material/web/MdFilledTextField";
 
 interface M3FilledTextFieldProps {
-  label?: string;
   disabled?: boolean;
+  error?: boolean;
+  errorText?: string;
+  label?: string;
+  required?: boolean;
+  value?: string;
+  prefixText?: string;
+  suffixText?: string;
+  supportingText?: string;
+  max?: string;
+  maxLength?: number;
+  min?: string;
+  minLength?: number;
+  pattern?: string;
+  placeholder?: string;
+  readOnly?: boolean;
 }
 
-export default function M3FilledSelect({disabled,label}: M3FilledTextFieldProps) {
-  return <FilledTextField label={label} disabled={disabled}></FilledTextField>;
+export default function M3FilledSelect({
+  disabled, error, errorText, label, required, value = '', prefixText, suffixText, supportingText, max, maxLength = -1,
+  min, minLength, pattern, placeholder, readOnly
+}: M3FilledTextFieldProps) {
+  return <FilledTextField
+    label={label} error={error} errorText={errorText} disabled={disabled} required={required} value={value}
+    prefixText={prefixText} suffixText={suffixText} supportingText={supportingText} max={max} maxLength={maxLength}
+    min={min} minLength={minLength} pattern={pattern} placeholder={placeholder} readOnly={readOnly}
+  />;
 }
--- src/material/react/M3OutlinedTextField.tsx
+++ src/material/react/M3OutlinedTextField.tsx
@@ -2,10 +2,31 @@ import * as React from "react";
 import OutlinedTextField from "material/web/MdOutlinedTextField";
 
 interface OutlinedTextFieldProps {
-  label?: string;
   disabled?: boolean;
+  error?: boolean;
+  errorText?: string;
+  label?: string;
+  required?: boolean;
+  value?: string;
+  prefixText?: string;
+  suffixText?: string;
+  supportingText?: string;
+  max?: string;
+  maxLength?: number;
+  min?: string;
+  minLength?: number;
+  pattern?: string;
+  placeholder?: string;
+  readOnly?: boolean;
 }
 
-export default function M3FilledSelect({disabled,label}: OutlinedTextFieldProps) {
-  return <OutlinedTextField label={label} disabled={disabled}></OutlinedTextField>;
+export default function M3FilledSelect({
+  disabled, error, errorText, label, required, value = '', prefixText, suffixText, supportingText, max, maxLength = -1,
+  min, minLength, pattern, placeholder, readOnly
+}: OutlinedTextFieldProps) {
+  return <OutlinedTextField
+    label={label} error={error} errorText={errorText} disabled={disabled} required={required} value={value}
+    prefixText={prefixText} suffixText={suffixText} supportingText={supportingText} max={max} maxLength={maxLength}
+    min={min} minLength={minLength} pattern={pattern} placeholder={placeholder} readOnly={readOnly}
+  />;
 }
