0f25df1a0e96721e71c673b0de33b77df0d59467
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2023-07-15 21:16:37 +0300

    Update Switch

--- src/material/react/M3Switch.tsx
+++ src/material/react/M3Switch.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 import Switch from "material/web/MdSwitch";
 
 interface M3SwitchProps {
+  disabled?: boolean;
   selected?: boolean;
   showIcon?: boolean;
+  icons?: boolean;
+  value?: string;
+  onClick?: ()=>void;
 }
 
-export default function M3Switch({ selected, showIcon }: M3SwitchProps) {
-  return <Switch selected={selected} showOnlySelectedIcon={showIcon} />;
+export default function M3Switch({ disabled, selected, showIcon, icons, value, onClick }: M3SwitchProps) {
+  return <Switch onClick={onClick} disabled={disabled} selected={selected} showOnlySelectedIcon={showIcon} icons={icons} value={value} />;
 }
