dfb6854eb67fa0ba326257fb6d4fa1f008eb7a51
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2023-07-15 21:21:16 +0300

    Update Radio

--- src/material/react/M3Radio.tsx
+++ src/material/react/M3Radio.tsx
@@ -4,8 +4,10 @@ import Radio from "material/web/MdRadio";
 interface M3RadioProps {
   checked?: boolean;
   disabled?: boolean;
+  value?: string;
+  onClick?: ()=>void;
 }
 
-export default function M3Radio({ checked, disabled }: M3RadioProps) {
-  return <Radio checked={checked} disabled={disabled} />;
+export default function M3Radio({ checked, disabled, value, onClick }: M3RadioProps) {
+  return <Radio onClick={onClick} checked={checked} disabled={disabled} value={value} />;
 }
