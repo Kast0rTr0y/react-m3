b0a9d5c2ba0a1f23e0637dea879be9af51ada61b
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2023-07-17 10:51:33 +0300

    Add checkValidity callback

--- src/material/react/M3FilledTextField.tsx
+++ src/material/react/M3FilledTextField.tsx
@@ -23,17 +23,19 @@ interface M3FilledTextFieldProps {
   style?: React.CSSProperties;
   type?: 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url';
   step?: string;
+  checkValidity?: (v:boolean)=>void
 }
 
 export default function M3FilledTextField({
   disabled, error, errorText, label, required, value = '', prefixText, suffixText, supportingText, max, maxLength = -1,
-  min, minLength, pattern, placeholder, readOnly, onChange, style, type, step
+  min, minLength, pattern, placeholder, readOnly, onChange, style, type, step, checkValidity
 }: M3FilledTextFieldProps) {
   const ref = useRef<MdFilledTextField>(null);
   function validate() {
     if(error === undefined) {
       ref.current?.reportValidity();
     }
+    if(typeof checkValidity === 'function' && ref.current) checkValidity(ref.current.checkValidity());
   }
   React.useEffect(()=>{
     validate();
--- src/material/react/M3OutlinedTextField.tsx
+++ src/material/react/M3OutlinedTextField.tsx
@@ -23,17 +23,19 @@ interface OutlinedTextFieldProps {
   style?: React.CSSProperties;
   type?: 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url';
   step?: string;
+  checkValidity?: (v:boolean)=>void
 }
 
 export default function M3OutlinedTextField({
   disabled, error, errorText, label, required, value = '', prefixText, suffixText, supportingText, max, maxLength = -1,
-  min, minLength, pattern, placeholder, readOnly, onChange, style, type, step
+  min, minLength, pattern, placeholder, readOnly, onChange, style, type, step, checkValidity
 }: OutlinedTextFieldProps) {
   const ref = useRef<MdOutlinedTextField>(null);
   function validate() {
     if(error === undefined) {
       ref.current?.reportValidity();
     }
+    if(typeof checkValidity === 'function' && ref.current) checkValidity(ref.current.checkValidity());
   }
   React.useEffect(()=>{
     validate();
